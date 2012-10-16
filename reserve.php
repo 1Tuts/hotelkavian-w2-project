6c811c303faf46770f0d2dce6f3483a5f3f9e7e0
soheil1999 <soheilsadabadi78@yahoo.com>
2012-10-16 17:21:49 +0330

    some copying

---
+++ reserve.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="fa-IR" dir="rtl">
+<head>
+	<meta charset="UTF-8" />
+	<title>..:: Hotel-Kavian ::..</title>
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script type="text/javascript" src="common-files/html5shiv.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="kadr">
+			<nav class="menu">
+				<menu>
+					<li><a href="index.php">صفحه اصلی</a></li>
+					<li><a href="reserve.php">رزرو اتاق</a></li>
+					<li><a href="contact.php">ارتباط با ما</a></li>
+					<li><a href="about.php">درباره ی ما</a></li>
+					
+				</menu>
+			</nav>
+			<div class="pichp"></div>
+			<div class="logo">
+				<h1>
+					<a href="#">
+						<img src="images/logo.jpg"/>
+					</a>
+				</h1>
+			</div>
+		</div>
+		<div class="language"></div>
+		<div class="copy"></div>
+	</div>
+</body>
+</html>
