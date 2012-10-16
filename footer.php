0ea6fe8300a30661fa241062f1391384393e10dd
soheil1999 <soheilsadabadi78@yahoo.com>
2012-10-16 17:50:45 +0330

    done the Parting

---
+++ footer.php
@@ -0,0 +1,13 @@
+			<div class="logo">
+				<h1>
+					<a href="#">
+						<img src="images/logo.jpg"/>
+					</a>
+				</h1>
+			</div>
+		</div>
+		<div class="language"></div>
+		<div class="copy"></div>
+	</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="fa-IR" dir="rtl">
+<head>
+	<meta charset="UTF-8" />
+	<title>..:: Hotel-Kavian ::..</title>
+	<link rel="stylesheet" type="text/css" href="1styles.css" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<script type="text/javascript" src="common-files/html5shiv.js"></script>
+</head>
+<body>
+	<div class="container">
