77a10a2941a9df75e92b707b16fb6cebc63e00ad
soheil1999 <soheilsadabadi78@yahoo.com>
2012-10-16 17:53:55 +0330

    made the css work

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <html lang="fa-IR" dir="rtl">
 <head>
 	<meta charset="UTF-8" />
-	<title>..:: Hotel-Kavian ::..</title>
-	<link rel="stylesheet" type="text/css" href="1styles.css" />
-	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title><?php bloginfo('name'); wp_title(); ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>\1styles.css" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>\style.css" />
 	<script type="text/javascript" src="common-files/html5shiv.js"></script>
 </head>
 <body>
